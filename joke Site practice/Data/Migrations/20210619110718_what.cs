43becf8f6de0ff33ccb777d2099c4f7b866eb2a7
Stan <slu40@uclive.ac.nz>
2021-06-19 23:12:35 +1200

    Add project files.

---
+++ joke Site practice/Data/Migrations/20210619110718_what.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace joke_Site_practice.Data.Migrations
+{
+    public partial class what : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "JokeEmail",
+                table: "Joke",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JokeEmail",
+                table: "Joke");
+        }
+    }
+}
---
+++ joke Site practice/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace joke_Site_practice.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
